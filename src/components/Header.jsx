a4eadf8a5ae236361ad9048600dca8fc453b2d62
PauloMBorges <paulodemeloborges@gmail.com>
2024-07-17 16:19:44 -0300

    Add content to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
-import trollFace from '../assets/troll-face.png';
+import trollFace from "../assets/troll-face.png";
 
 function Header() {
-    return (
-        <header>
-            <img src={trollFace} alt="troll face" />
-            <h2></h2>
-        </header>
-    );
+  return (
+    <header className="header">
+      <img className="header--image" src={trollFace} alt="troll face" />
+      <h2 className="header--title">Meme Generator</h2>
+      <h4 className="header--project">React Course - Project 3</h4>
+    </header>
+  );
 }
 
 export default Header;
