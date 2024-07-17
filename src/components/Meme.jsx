6e2fd452c30a8be840955719612e1aca88a3f130
PauloMBorges <paulodemeloborges@gmail.com>
2024-07-17 16:56:24 -0300

    Update: a11y audit

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,8 +2,26 @@ function Meme() {
   return (
     <main>
       <form className="form">
-        <input className="form--input" type="text" placeholder="Top text" />
-        <input className="form--input" type="text" placeholder="Bottom text" />
+        <div>
+          <label>
+            Top text
+            <input
+              className="form--input"
+              type="text"
+              placeholder="Shut up"
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Bottom text
+            <input
+              className="form--input"
+              type="text"
+              placeholder="and take my money"
+            />
+          </label>
+        </div>
         <button className="form--button">Get a new meme image 📸</button>
       </form>
     </main>
