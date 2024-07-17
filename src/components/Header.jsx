0f55d70563f70ff7561f5c5d964deb02cc19fd4a
PauloMBorges <paulodemeloborges@gmail.com>
2024-07-17 16:17:06 -0300

    Create Header component and add trollface asset

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import trollFace from '../assets/troll-face.png';
+
+function Header() {
+    return (
+        <header>
+            <img src={trollFace} alt="troll face" />
+            <h2></h2>
+        </header>
+    );
+}
+
+export default Header;
