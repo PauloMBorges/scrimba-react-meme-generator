6c18d74f79b7b71caaed0e10baa3fce634475eaf
PauloMBorges <paulodemeloborges@gmail.com>
2024-07-19 10:25:25 -0300

    Add useState to memeImage

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 function Meme() {
+  const [memeImage, setMemeImage] = useState("http://i.imgflip.com/1bij.jpg");
+
   return (
     <main>
       <div className="form">
@@ -6,6 +10,7 @@ function Meme() {
         <input type="text" placeholder="Bottom text" className="form--input" />
         <button className="form--button">Get a new meme image 🖼</button>
       </div>
+      <img src={memeImage} alt="" className="meme--image" />
     </main>
   );
 }
