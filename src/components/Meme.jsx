a54cbee9090b102d2ad9ca9309a91e0e6726aef2
PauloMBorges <paulodemeloborges@gmail.com>
2024-07-19 10:28:29 -0300

    Add getMemeImage() to get random meme img onClick

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
+import memesData from "../memesData";
 
 function Meme() {
   const [memeImage, setMemeImage] = useState("http://i.imgflip.com/1bij.jpg");
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
 
   return (
     <main>
       <div className="form">
         <input type="text" placeholder="Top text" className="form--input" />
         <input type="text" placeholder="Bottom text" className="form--input" />
-        <button className="form--button">Get a new meme image 🖼</button>
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
       </div>
       <img src={memeImage} alt="" className="meme--image" />
     </main>
